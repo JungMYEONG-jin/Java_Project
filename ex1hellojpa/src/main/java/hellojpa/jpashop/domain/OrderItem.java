81228ad6fa50af2058c3dd02204ddbfb13d48979
godcoder <dwrhksfl77@naver.com>
2022-03-10 22:58:42 +0900

    study

---
+++ ex1hellojpa/src/main/java/hellojpa/jpashop/domain/OrderItem.java
@@ -0,0 +1,62 @@
+package hellojpa.jpashop.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="ORDER_IO")
+    private Order order;
+
+    @Column(name="ITEM_ID")
+    private Long itemId;
+
+    private int orderPrice;
+    private int count;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(int orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
