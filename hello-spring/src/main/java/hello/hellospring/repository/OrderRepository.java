9d554a9b11aa5d0b63af5bb7bdb6275fada54785
MJ1996 <55305549+JungMYEONG-jin@users.noreply.github.com>
2022-04-05 09:56:07 +0900

    Update OrderRepository.java

--- hello-spring/src/main/java/hello/hellospring/repository/OrderRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/OrderRepository.java
@@ -29,4 +29,52 @@ public class OrderRepository {
     {
         return em.createQuery("select o from Order o join o.member m", Order.class).getResultList();
     }
+    
+    public List<Order> findAll(OrderSearch orderSearch)
+    {
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirst = true;
+
+        if(orderSearch.getOrderStatus()!=null)
+        {
+            if(isFirst)
+            {
+                jpql+=" where";
+                isFirst = false;
+            }else
+            {
+                jpql+=" and";
+            }
+            jpql+=" o.status = :status";
+        }
+
+        if(StringUtils.hasText(orderSearch.getMemberName()))
+        {
+            if(isFirst)
+            {
+                jpql+=" where";
+                isFirst = false;
+            }else
+            {
+                jpql+=" and";
+            }
+            jpql+=" m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class).setMaxResults(1000);
+
+        if(orderSearch.getOrderStatus()!=null)
+        {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+
+        if(StringUtils.hasText(orderSearch.getMemberName()))
+        {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+
+        return query.getResultList();
+
+
+    }
 }
