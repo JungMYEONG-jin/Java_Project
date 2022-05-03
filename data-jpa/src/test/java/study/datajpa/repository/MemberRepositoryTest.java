8954c6202214d2449deaa249a8859d32b4be91f4
godcoder <dwrhksfl77@naver.com>
2022-05-03 23:23:08 +0900

    JPA interface 적용

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,11 +27,45 @@ class MemberRepositoryTest {
     {
         Member member = new Member("AA");
         Member savedMember = memberRepository.save(member);
-        Member
-                findMember = memberRepository.findById(savedMember.getId()).get();
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
 
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         Assertions.assertThat(findMember).isEqualTo(member);
     }
+
+    @Test
+    void basicCRUD()
+    {
+        Member member1 = new Member("A");
+        Member member2 = new Member("B");
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        // single result
+        Assertions.assertThat(findMember1).isEqualTo(member1);
+        Assertions.assertThat(findMember2).isEqualTo(member2);
+
+        // list result
+
+        List<Member> all = memberRepository.findAll();
+        Assertions.assertThat(all.size()).isEqualTo(2);
+
+        // 카운트 확인
+        long count = memberRepository.count();
+        Assertions.assertThat(count).isEqualTo(2);
+
+        memberRepository.deleteAll();
+
+        // 삭제 후 카운트 확인
+        long Dcount = memberRepository.count();
+        Assertions.assertThat(Dcount).isEqualTo(0);
+
+    }
+
+
 }
