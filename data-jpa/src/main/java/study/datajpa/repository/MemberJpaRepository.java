dab6ddb1ec62722840f3eefeedd8b7b49d6326a0
godcoder <dwrhksfl77@naver.com>
2022-05-09 22:41:32 +0900

    JPA Named Query

--- data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -47,5 +47,4 @@ public class MemberJpaRepository {
         return em.createQuery("select m from Member m where m.username = :username and m.age > :age").setParameter("username", username).setParameter("age", age).getResultList();
     }
 
-
 }
--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,13 +1,14 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    List<Member> findByUsername(String username);
+    List<Member> findByUsername(@Param("username") String username);
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 }
