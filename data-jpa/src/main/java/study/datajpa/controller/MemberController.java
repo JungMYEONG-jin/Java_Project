3190bf21af0ee539d26f68c2fd8863d89e4b0be2
godcoder <dwrhksfl77@naver.com>
2022-05-15 23:46:38 +0900

    web pageable!! 인덱스는 0부터..

--- data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,14 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -29,10 +34,28 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<Member> list(@PageableDefault(size=5) Pageable pageable)
+    {
+        return memberRepository.findAll(pageable);
+    }
+
+
+    // 절대 entity 자체를 반환하면 안됨. 위험!
+    @GetMapping("/membersDto")
+    public Page<MemberDto> listDto(@PageableDefault(size=5) Pageable pageable)
+    {
+        return memberRepository.findAll(pageable).map(m -> new MemberDto(m.getId(), m.getUsername(), "teamA"));
+    }
+
+
     @PostConstruct
     public void init()
     {
-        memberRepository.save(new Member("userA"));
+        for(int i=0;i<100;i++)
+        {
+            memberRepository.save(new Member("user"+i, i));
+        }
     }
 
 }
