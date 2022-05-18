7074217ea9b9f13624ad8cee411e4ea10d7a06ba
a60156077 <dwrhksfl77@naver.com>
2022-05-18 17:58:09 +0900

    sol util

--- getCI/src/main/java/sol/com/shinhan/filter/SupFilterSessionCheck.java
+++ getCI/src/main/java/sol/com/shinhan/filter/SupFilterSessionCheck.java
@@ -1,11 +1,21 @@
 package sol.com.shinhan.filter;
 
+import org.apache.commons.io.IOUtils;
+import org.bouncycastle.util.encoders.Base64Encoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sol.com.shinhan.superplatform.util.SUPSecureUtil;
+import sol.com.shinhan.superplatform.util.SUPStringUtil;
+import sun.misc.BASE64Encoder;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class SupFilterSessionCheck implements Filter {
+
+    Logger logger = LoggerFactory.getLogger(SupFilterSessionCheck.class);
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         Filter.super.init(filterConfig);
@@ -29,6 +39,16 @@ public class SupFilterSessionCheck implements Filter {
                 SupHttpRequestWrapper requestWrapper = new SupHttpRequestWrapper(req);
 
                 //hash check
+                String hashData = SUPStringUtil.null2StrTrim(req.getHeader("body-vdata"));
+
+                String bodyData = IOUtils.toString(req.getInputStream(), "UTF-8");
+
+                logger.info("doFilter bodyData : "+bodyData);
+
+                byte[] hash = SUPSecureUtil.hashSHA256(bodyData);
+
+                BASE64Encoder base64Encoder = new BASE64Encoder();
+                String hashBodyData = new String(base64Encoder.encode(hash));
             }
 
         }
