5882f8d7a3adcf1478dd0c790617890d1537bae3
a60156077 <dwrhksfl77@naver.com>
2022-05-19 13:47:35 +0900

    sol controller

--- getCI/src/main/java/sol/com/shinhan/controller/DataInitController.java
+++ getCI/src/main/java/sol/com/shinhan/controller/DataInitController.java
@@ -1,5 +1,7 @@
 package sol.com.shinhan.controller;
 
+import org.json.simple.JSONObject;
+
 public abstract class DataInitController {
 
     public boolean inOutLogData = false;
@@ -25,6 +27,39 @@ public abstract class DataInitController {
     public void dataException(NoClassDefFoundError e)
     {
         WARNINGMsg w = new WARINGMsg();
+        w.msg = e.getMessage();
+
+        if(w.msg == null || w.msg.trim().equals("")){
+            w.msg = "Exception occurred";
+            w.detail = RES.EXCEPTION_DETAIL_MSG;
+        }else if(w.msg.indexOf("ORA-") > -1){
+            w.msg = "DB Error occurred";
+            w.detail = "Retry it";
+        }else{
+            w.msg = "Exception occurred";
+            w.detail = RES.EXCEPTION_DETAIL_MSG;
+        }
+
+        JSONObject errorObj = new JSONObject();
+
+        errorObj.put("msg", w.msg);
+        errorObj.put("detail", w.detail);
+
+
+    }
+
+    public JSONObject noDataException()
+    {
+        JSONObject head = new JSONObject();
+        JSONObject body = new JSONObject();
+        JSONObject data = new JSONObject();
+
+        head.put("result", "FAIL");
+        head.put("resultMSG", "param data null");
+        data.put("header", head);
+        data.put("body", body);
+
+        return data;
     }
 
 
