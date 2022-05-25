65cb8fc3b9fa36c3f18ce811f5dbbb40121fe075
godcoder <dwrhksfl77@naver.com>
2022-05-25 23:47:37 +0900

    trace2

---
+++ advanced/src/main/java/hello/advanced/app/v1/OrderControllerV1.java
@@ -0,0 +1,32 @@
+package hello.advanced.app.v1;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // controller + response body
+@RequiredArgsConstructor
+public class OrderControllerV1 {
+
+    private final OrderServiceV1 orderService;
+    private final HelloTraceV1 trace; // DI
+
+    @GetMapping("/v1/request")
+    public String request(String itemId){
+
+        TraceStatus status = null;
+        try{
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(itemId);
+            trace.end(status);
+            return "OK";
+        }catch (Exception e){
+            trace.exception(status, e);
+            throw e; // 예외를 꼭 반환해야함.
+        }
+
+    }
+
+}
