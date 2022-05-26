59f6b5a75c1be0f78b2b6592110a8f8c9a857138
a60156077 <dwrhksfl77@naver.com>
2022-05-26 17:04:38 +0900

    H2를 이용한 내장타입 db 사용하기

--- getCI/src/test/java/springbook/issuetracker/sqlservice/updatable/EmbeddedSqlRegistryTest.java
+++ getCI/src/test/java/springbook/issuetracker/sqlservice/updatable/EmbeddedSqlRegistryTest.java
@@ -1,12 +1,33 @@
 package springbook.issuetracker.sqlservice.updatable;
 
+import org.junit.After;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import springbook.issuetracker.sqlservice.UpdatableSqlRegistry;
+import springbook.registry.AbstractUpdatableSqlRegistryTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest(classes = Runnable.class)
-class EmbeddedSqlRegistryTest {
+public class EmbeddedSqlRegistryTest extends AbstractUpdatableSqlRegistryTest {
 
+    EmbeddedDatabase db;
+
+    @Override
+    protected UpdatableSqlRegistry createUpdatableSqlRegistry() {
+        db = new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).addScript("../resources/schema.sql").build();
+
+        EmbeddedSqlRegistry embeddedSqlRegistry = new EmbeddedSqlRegistry();
+        embeddedSqlRegistry.setDataSource(db);
+
+        return embeddedSqlRegistry;
+    }
+
+    @After
+    public void tearDown(){
+        db.shutdown();
+    }
 
 
 
