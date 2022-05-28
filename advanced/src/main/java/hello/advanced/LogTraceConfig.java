78ee3cc5a4249d07d329de67a9da60e489a2b759
godcoder <dwrhksfl77@naver.com>
2022-05-28 23:53:49 +0900

    config 수정

--- advanced/src/main/java/hello/advanced/LogTraceConfig.java
+++ advanced/src/main/java/hello/advanced/LogTraceConfig.java
@@ -2,6 +2,7 @@ package hello.advanced;
 
 import hello.advanced.trace.logtrace.FieldLogTrace;
 import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,6 @@ public class LogTraceConfig {
     // 싱글톤인데 쓰레드는 여러개임. 여러 유저가 요청하면 여러 쓰레드가 동시에 접근해서 동시성 문제가 발생
     @Bean
     public LogTrace logTrace(){
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
