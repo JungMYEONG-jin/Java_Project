cf9e7773b9d11ab6f95bd08f1d234c6b425b90a6
godcoder <dwrhksfl77@naver.com>
2022-05-28 09:16:46 +0900

    동시성 문제 발생..

--- advanced/src/main/java/hello/advanced/LogTraceConfig.java
+++ advanced/src/main/java/hello/advanced/LogTraceConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class LogTraceConfig {
 
+    // 싱글톤인데 쓰레드는 여러개임. 여러 유저가 요청하면 여러 쓰레드가 동시에 접근해서 동시성 문제가 발생
     @Bean
     public LogTrace logTrace(){
         return new FieldLogTrace();
