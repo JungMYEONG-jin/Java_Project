31f59b53baaca05d065f769e5712713cc41c6f7a
a60156077 <dwrhksfl77@naver.com>
2022-05-31 13:42:09 +0900

    Proxy Pattern v2 CacheProxy 추가

--- data-jpa/src/test/java/study/datajpa/pureproxy/proxy/ProxyPatternTest.java
+++ data-jpa/src/test/java/study/datajpa/pureproxy/proxy/ProxyPatternTest.java
@@ -1,8 +1,10 @@
 package study.datajpa.pureproxy.proxy;
 
 import org.junit.jupiter.api.Test;
+import study.datajpa.pureproxy.proxy.code.CacheProxy;
 import study.datajpa.pureproxy.proxy.code.ProxyPatternClient;
 import study.datajpa.pureproxy.proxy.code.RealSubject;
+import study.datajpa.pureproxy.proxy.code.Subject;
 
 public class ProxyPatternTest {
 
@@ -10,6 +12,17 @@ public class ProxyPatternTest {
     void noProxyTest(){
         RealSubject realSubject = new RealSubject();
         ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+
+    }
+
+    @Test
+    void cacheProxyTest(){
+        Subject realSubject = new RealSubject();
+        Subject cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
         client.execute();
         client.execute();
         client.execute();
---
+++ data-jpa/src/test/java/study/datajpa/pureproxy/proxy/code/CacheProxy.java
@@ -0,0 +1,23 @@
+package study.datajpa.pureproxy.proxy.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CacheProxy implements Subject{
+
+    private Subject subject;
+    private String cacheValue;
+
+    public CacheProxy(Subject subject) {
+        this.subject = subject;
+    }
+
+    @Override
+    public String operation() {
+        log.info("proxy call");
+        if (cacheValue == null){
+            cacheValue = subject.operation();
+        }
+        return cacheValue;
+    }
+}
