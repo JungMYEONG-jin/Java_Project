a511e6b544e6273ab409bc54ed961a1855dc0635
a60156077 <dwrhksfl77@naver.com>
2022-05-31 15:19:01 +0900

    구체 클래스에 proxy 적용

---
+++ data-jpa/src/test/java/study/datajpa/pureproxy/concreteproxy/ConcreteProxyTest.java
@@ -0,0 +1,24 @@
+package study.datajpa.pureproxy.concreteproxy;
+
+import org.junit.jupiter.api.Test;
+import study.datajpa.pureproxy.concreteproxy.code.ConcreteClient;
+import study.datajpa.pureproxy.concreteproxy.code.ConcreteLogic;
+import study.datajpa.pureproxy.concreteproxy.code.TimeProxy;
+
+public class ConcreteProxyTest {
+
+    @Test
+    void noProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        ConcreteClient client = new ConcreteClient(concreteLogic);
+        client.execute();
+    }
+
+    @Test
+    void addProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        TimeProxy timeProxy = new TimeProxy(concreteLogic);
+        ConcreteClient client = new ConcreteClient(timeProxy);
+        client.execute();
+    }
+}
