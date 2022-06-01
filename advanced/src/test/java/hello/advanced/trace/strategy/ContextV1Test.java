5665240d9c8201668fa61971bc80674098a2fb5e
godcoder <dwrhksfl77@naver.com>
2022-06-01 23:22:25 +0900

    strategy pattern v3

--- advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,6 +1,7 @@
 package hello.advanced.trace.strategy;
 
 import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import hello.advanced.trace.template.code.AbstractTemplate;
@@ -27,6 +28,45 @@ public class ContextV1Test {
         context.execute();
 
     }
+
+    /**
+     * 익명함수
+     */
+    @Test
+    void strategyV2() {
+        ContextV1 context = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("business logic 1");
+            }
+        });
+
+        ContextV1 context2 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("business logic 2");
+            }
+        });
+
+        context.execute();
+        context2.execute();
+
+    }
+
+    /**
+     * 람다로 간편하게 줄이기
+     */
+    @Test
+    void strategyV3() {
+        ContextV1 context = new ContextV1(()->log.info("business logic 1"));
+        ContextV1 context2 = new ContextV1(() -> log.info("business logic 2"));
+
+        context.execute();
+        context2.execute();
+
+    }
+
+
 
     private void logic1(){
         long startTime = System.currentTimeMillis();
