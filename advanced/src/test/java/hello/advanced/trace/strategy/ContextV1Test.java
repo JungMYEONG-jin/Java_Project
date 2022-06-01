e0fe297ae04393427a317ea17cba5277c2fe25e9
godcoder <dwrhksfl77@naver.com>
2022-06-01 22:59:00 +0900

    strategy pattern v0

---
+++ advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -0,0 +1,51 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.template.code.AbstractTemplate;
+import hello.advanced.trace.template.code.SubClassLogic1;
+import hello.advanced.trace.template.code.SubClassLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV1Test {
+
+    @Test
+    void strategyV0() {
+        logic1();
+        logic2();
+    }
+
+    private void logic1(){
+        long startTime = System.currentTimeMillis();
+
+        log.info("logic1 is running");
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+
+        log.info("resultTime={}", resultTime);
+    }
+
+    private void logic2(){
+        long startTime = System.currentTimeMillis();
+
+        log.info("logic2 is running");
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+
+        log.info("resultTime={}", resultTime);
+    }
+
+
+    // template method pattern
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+        AbstractTemplate template2 = new SubClassLogic2();
+        template1.execute();
+        template2.execute();
+    }
+
+
+}
