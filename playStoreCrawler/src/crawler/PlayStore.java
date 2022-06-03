bd7fb00ec6e86ccf2b518976e187bbc43423f464
a60156077 <dwrhksfl77@naver.com>
2022-06-03 10:57:51 +0900

    selenium appstore 정보 가져오기

--- playStoreCrawler/src/crawler/PlayStore.java
+++ playStoreCrawler/src/crawler/PlayStore.java
@@ -1,8 +1,13 @@
 package crawler;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
 
 public class PlayStore {
 
@@ -14,13 +19,56 @@ public class PlayStore {
 
         WebDriver driver = new ChromeDriver(chromeOptions);
 
-        String naver = "https://www.naver.com";
+//        String naver = "https://www.naver.com";
 
-        driver.get(naver);
-        String pageSource = driver.getPageSource();
-        System.out.println("pageSource = " + pageSource);
+        String solURL = "https://play.google.com/store/apps/details?id=com.shinhan.sbanking&hl=ko&gl=US";
+
+        driver.get(solURL);
+
+        List<WebElement> elements = driver.findElements(By.xpath("//div[@class='h3YV2d']"));
+
+        WebElement element = driver.findElement(By.xpath("//button[@aria-label='앱 정보 자세히 알아보기']"));
+        if (element.isEnabled()){
+            System.out.println("클릭이 가능합니다.");
+
+            element.click();
+
+            sleep(1000);
+
+
+            WebElement title = driver.findElement(By.xpath("//*[@id=\"yDmH0d\"]/div[4]/div[2]/div/div/div/div/div[1]/div/div/h5[@class='xzVNx']"));
+            System.out.println("title.getText() = " + title.getText());
+
+            WebElement parentAppInfos = driver.findElement(By.xpath("//div[@class='G1zzid']"));
+            List<WebElement> appInfos = parentAppInfos.findElements(By.xpath("*"));
+
+            System.out.println("appInfos.size() = " + appInfos.size());
+            for (WebElement webElement : appInfos) {
+                // div class "q078ud" key
+                // div class "reAt0" value
+                WebElement key = webElement.findElement(By.xpath("div[@class='q078ud']"));
+                WebElement value = webElement.findElement(By.xpath("div[@class='reAt0']"));
+                System.out.println("key.getText() = " + key.getText());
+                System.out.println("value.getText() = "+value.getText());
+
+            }
+
+        }else{
+            System.out.println("클릭이 불가능합니다. xpath 확인해주세요.");
+        }
+
+
+
         driver.quit();
 
 
+    }
+
+    private static void sleep(int millis){
+        try{
+            Thread.sleep(millis);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
     }
 }
