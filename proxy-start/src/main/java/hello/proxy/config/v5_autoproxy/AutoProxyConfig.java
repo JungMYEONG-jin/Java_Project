940760328a272646828ac489be8a533a9d952f54
godcoder <dwrhksfl77@naver.com>
2022-06-15 22:58:02 +0900

    Advisor를 빈 등록시 자동 빈 등록함

--- proxy-start/src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
+++ proxy-start/src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -6,6 +6,7 @@ import hello.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
 import hello.proxy.trace.logtrace.LogTrace;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
@@ -17,10 +18,30 @@ import org.springframework.context.annotation.Import;
 public class AutoProxyConfig {
 
     // advisor만 빈으로 등록하면 자동으로 auto proxy해서 등록됨..
-    @Bean
+//    @Bean
     public Advisor advisor1(LogTrace trace){
         NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
         pointcut.setMappedNames("request*", "order*", "save*");
+        LogTraceAdvice advice = new LogTraceAdvice(trace);
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+//    @Bean
+    public Advisor advisor2(LogTrace trace){
+       //pointcut
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* hello.proxy.app..*(..))"); // 이 위치에 있어야 proxy 적용 대상이다.
+        //advice
+        LogTraceAdvice advice = new LogTraceAdvice(trace);
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+    @Bean
+    public Advisor advisor3(LogTrace trace){
+       //pointcut
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* hello.proxy.app..*(..)) && !execution(* hello.proxy.app..noLog(..))"); // 이 위치에 있어야 proxy 적용 대상이다.
+        //advice
         LogTraceAdvice advice = new LogTraceAdvice(trace);
         return new DefaultPointcutAdvisor(pointcut, advice);
     }
