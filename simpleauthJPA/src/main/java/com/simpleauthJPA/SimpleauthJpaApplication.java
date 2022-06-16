e6aced03cf2faef6af4b3f23210ae7af3d29b6a5
a60156077 <dwrhksfl77@naver.com>
2022-06-16 09:17:06 +0900

    init

---
+++ simpleauthJPA/src/main/java/com/simpleauthJPA/SimpleauthJpaApplication.java
@@ -0,0 +1,13 @@
+package com.simpleauthJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleauthJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleauthJpaApplication.class, args);
+	}
+
+}
