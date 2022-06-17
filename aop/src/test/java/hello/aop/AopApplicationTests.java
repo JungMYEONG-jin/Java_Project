9a4c3bb5ae8b7da5f44bf6864259286f3bc989e5
a60156077 <dwrhksfl77@naver.com>
2022-06-17 15:54:49 +0900

    aop project init v2

--- aop/src/test/java/hello/aop/AopApplicationTests.java
+++ aop/src/test/java/hello/aop/AopApplicationTests.java
@@ -1,10 +1,42 @@
 package hello.aop;
 
+import hello.aop.order.OrderRepository;
+import hello.aop.order.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import net.bytebuddy.pool.TypePool;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class AopApplicationTests {
+
+	@Autowired
+	OrderRepository orderRepository;
+
+	@Autowired
+	OrderService orderService;
+
+
+	@Test
+	void aopInfo() {
+		log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
+		log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+
+	}
+
+	@Test
+	void success() {
+		orderService.orderItem("itemA");
+	}
+
+	@Test
+	void exception() {
+		Assertions.assertThatThrownBy(() -> orderService.orderItem("ex")).isInstanceOf(IllegalStateException.class);
+	}
 
 	@Test
 	void contextLoads() {
