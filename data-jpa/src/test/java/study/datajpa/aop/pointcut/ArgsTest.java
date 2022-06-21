11812f00ec1875d848fe251a781a95362b425979
a60156077 <dwrhksfl77@naver.com>
2022-06-21 14:25:50 +0900

    aop args test

---
+++ data-jpa/src/test/java/study/datajpa/aop/pointcut/ArgsTest.java
@@ -0,0 +1,54 @@
+package study.datajpa.aop.pointcut;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import study.datajpa.aop.member.MemberServiceImpl;
+
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// args는 부모 타입 허용, execution은 파라미터 타입이 정확하게 매칭되어야 한다.
+
+public class ArgsTest {
+
+    Method helloMethod;
+
+    @BeforeEach
+    public void init() throws NoSuchMethodException{
+        helloMethod = MemberServiceImpl.class.getMethod("hello", String.class);
+    }
+
+    private AspectJExpressionPointcut pointcut(String expression){
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression(expression);
+        return pointcut;
+    }
+
+    @Test
+    void args() {
+        assertThat(pointcut("args(String").matches(helloMethod, MemberServiceImpl.class)).isTrue();
+        assertThat(pointcut("args(Object").matches(helloMethod, MemberServiceImpl.class)).isTrue();
+        assertThat(pointcut("args()").matches(helloMethod, MemberServiceImpl.class)).isFalse();
+        assertThat(pointcut("args(*)").matches(helloMethod, MemberServiceImpl.class)).isTrue();
+        assertThat(pointcut("args(..)").matches(helloMethod, MemberServiceImpl.class)).isTrue();
+        assertThat(pointcut("args(String,..)").matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    /**
+     * execution 정적
+     * args 동적
+     */
+    @Test
+    void argsVsExecution() {
+        assertThat(pointcut("args(String)").matches(helloMethod, MemberServiceImpl.class)).isTrue();
+        assertThat(pointcut("args(java.io.Serializable)").matches(helloMethod, MemberServiceImpl.class)).isTrue();
+        assertThat(pointcut("args(Object)").matches(helloMethod, MemberServiceImpl.class)).isTrue();
+
+        //execution
+        assertThat(pointcut("execution(* *(String))").matches(helloMethod, MemberServiceImpl.class)).isTrue();
+        assertThat(pointcut("execution(* *(java.io.Serializable))").matches(helloMethod, MemberServiceImpl.class)).isFalse();
+        assertThat(pointcut("execution(* *(Object))").matches(helloMethod, MemberServiceImpl.class)).isFalse();
+    }
+}
