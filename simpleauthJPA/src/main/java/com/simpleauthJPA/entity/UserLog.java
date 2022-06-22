5d967066f373b3f33bb7339c5fe7d901e3d9b6a1
a60156077 <dwrhksfl77@naver.com>
2022-06-22 14:24:09 +0900

    Add UserLog Entity

---
+++ simpleauthJPA/src/main/java/com/simpleauthJPA/entity/UserLog.java
@@ -0,0 +1,53 @@
+package com.simpleauthJPA.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "mbi_simpleauth_log")
+@Getter
+@Setter
+public class UserLog {
+    @Id
+    private Long seq;
+
+    @Column
+    private String cusno;
+    @Column
+    private String id;
+    @Column
+    private String tag;
+    @Column
+    private String injson;
+    @Column
+    private String outjson;
+    @Column
+    private String msg;
+    @Column
+    private String code;
+    @Column
+    private String stacktrace;
+    @Column
+    private String timestamp;
+
+    public UserLog() {
+    }
+
+    public UserLog(String cusno, String id, String tag, String injson, String outjson, String msg, String code, String stacktrace, String timestamp) {
+        this.cusno = cusno;
+        this.id = id;
+        this.tag = tag;
+        this.injson = injson;
+        this.outjson = outjson;
+        this.msg = msg;
+        this.code = code;
+        this.stacktrace = stacktrace;
+        this.timestamp = timestamp;
+    }
+}
