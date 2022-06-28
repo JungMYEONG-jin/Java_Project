ce034cac011d52b7e694ed79f980786d55d9e9e9
a60156077 <dwrhksfl77@naver.com>
2022-06-28 16:53:31 +0900

    instagram cloning v1.1.6
    - AuthService 추가.
    - Script 추가.

--- instagram/src/main/java/com/instagram/service/AuthService.java
+++ instagram/src/main/java/com/instagram/service/AuthService.java
@@ -1,4 +1,34 @@
 package com.instagram.service;
 
+import com.instagram.entity.User;
+import com.instagram.handler.exception.CustomAPIException;
+import com.instagram.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    /**
+     * repository에 저장하지 않은 유저여야 함!!
+     * 굳이 안쓰고 userService에서 처리해도 될듯함.
+     * 그렇지 않으면 패스워드가 무의미함.
+     * @param user
+     * @return
+     */
+    @Transactional
+    public User join(User user){
+            String rawPassword = user.getPassword();
+            String encodedPassword = passwordEncoder.encode(rawPassword);
+            user.setPassword(encodedPassword);
+            user.setRole("Normal_USER");
+            User savedUser = userRepository.save(user);
+            return savedUser;
+    }
 }
--- instagram/src/main/java/com/instagram/service/CommentService.java
+++ instagram/src/main/java/com/instagram/service/CommentService.java
@@ -1,14 +1,35 @@
 package com.instagram.service;
 
+import com.instagram.entity.Comment;
+import com.instagram.entity.Image;
+import com.instagram.entity.User;
+import com.instagram.handler.exception.CustomAPIException;
 import com.instagram.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
 
     private final CommentRepository commentRepository;
+
+    @Transactional
+    public Comment write(String content, Image image, User user){
+        Comment comment = Comment.builder().user(user).image(image).content(content).build();
+        commentRepository.save(comment);
+        return comment;
+    }
+
+
+    public void unComment(long id){
+        try{
+            commentRepository.deleteById(id);
+        }catch (Exception e){
+            throw new CustomAPIException(e.getMessage());
+        }
+    }
 }
 
 
