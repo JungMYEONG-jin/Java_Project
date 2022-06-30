c7aa749093a1186eae11569050bad5762c34c77e
a60156077 <dwrhksfl77@naver.com>
2022-06-30 10:59:15 +0900

    instagram cloning v1.1.9
    - 기존 WebSecurityConfigurerAdapter  deprecated
    - 대체 방법 SecurityFilterChain 구현.

---
+++ instagram/src/main/java/com/instagram/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.instagram.config;
+
+import com.instagram.config.oauth.OAuth2DetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    private final OAuth2DetailsService oAuth2DetailsService;
+
+    @Bean
+    public BCryptPasswordEncoder encode(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("/", "/user/**", "/image/**", "/subscribe/**", "/comment/**", "/api/**")
+                .authenticated().anyRequest().permitAll()
+                .and().formLogin().loginPage("/auth/signin") // Get 인증이 필요한 페이지에 접근 시 호출 페이지
+                .loginProcessingUrl("/auth/signin")
+                .defaultSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()// code, access_token 받는 과정 생략하고 회원정보 바로 받을 수 있게
+                .userService(oAuth2DetailsService);
+        http.headers().frameOptions().sameOrigin();
+        return http.build();
+    }
+}
