6967363fd1193867704e77181d50e102a5fd828d
a60156077 <dwrhksfl77@naver.com>
2022-07-05 17:08:56 +0900

    instagram cloning v1.3.9
    - AuthController email 형식 검증 추가.
    - signup 페이지 thymeleaf 검증 추가.

--- instagram/src/main/java/com/instagram/controller/AuthController.java
+++ instagram/src/main/java/com/instagram/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.instagram.controller;
 import com.instagram.dto.signup.SignupDto;
 import com.instagram.entity.User;
 import com.instagram.service.AuthService;
+import com.instagram.util.ValidUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -16,6 +17,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -30,16 +33,17 @@ public class AuthController {
     }
 
     @GetMapping("/auth/signup")
-    public String signupForm(){
+    public String signupForm(@ModelAttribute SignupDto signupDto){
         return "auth/signup";
     }
 
     @PostMapping("/auth/signup")
-    public String signup(@ModelAttribute SignupDto signupDto, BindingResult bindingResult){
+    public String signup(@ModelAttribute("signupDto") SignupDto signupDto, BindingResult bindingResult){
 
         User user = signupDto.toEntity();
-        if(!user.getEmail().contains("@")){
-            bindingResult.addError(new FieldError("signupDto", "email", "잘못된 이메일 형식입니다. 다시 입력하세요."));
+
+        if (!ValidUtils.isValidEmail(user.getEmail())){
+            bindingResult.addError(new FieldError("signupDto", "email", "잘못된 이메일 형식입니다."));
         }
 
         if (bindingResult.hasErrors()){
@@ -51,4 +55,5 @@ public class AuthController {
         authService.join(user);
         return "auth/signin";
     }
+
 }
---
+++ instagram/src/main/java/com/instagram/util/ValidUtils.java
@@ -0,0 +1,13 @@
+package com.instagram.util;
+
+import java.util.regex.Pattern;
+
+public class ValidUtils {
+
+    public static boolean isValidEmail(String email){
+        String regex = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+                + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+        return Pattern.compile(regex).matcher(email).matches();
+    }
+
+}
