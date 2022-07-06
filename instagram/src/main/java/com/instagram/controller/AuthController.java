78a97b188e19e346b000c0445e087897dd0b6e4f
a60156077 <dwrhksfl77@naver.com>
2022-07-06 13:27:44 +0900

    instagram cloning v1.4.1
    - 에러 페이지 처리

--- instagram/src/main/java/com/instagram/controller/AuthController.java
+++ instagram/src/main/java/com/instagram/controller/AuthController.java
@@ -7,18 +7,12 @@ import com.instagram.util.ValidUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.validation.Valid;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Slf4j
 @RequiredArgsConstructor
