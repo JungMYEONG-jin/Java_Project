6b58b322e63e9a1c6d3e28a046e34d4e65795510
a60156077 <dwrhksfl77@naver.com>
2022-07-08 16:48:52 +0900

    ecommerce project
    - UserService 구현.

---
+++ ecommerce/src/main/java/com/ecommerce/common/utils/ModelMapperUtils.java
@@ -0,0 +1,18 @@
+package com.ecommerce.common.utils;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+
+public class ModelMapperUtils {
+
+    private static ModelMapper modelMapper;
+
+    public static ModelMapper getModelMapper(){
+        if(modelMapper == null){
+            modelMapper = new ModelMapper();
+            modelMapper.getConfiguration().setSkipNullEnabled(true)
+                    .setMatchingStrategy(MatchingStrategies.STRICT);
+        }
+        return modelMapper;
+    }
+}
---
+++ ecommerce/src/main/java/com/ecommerce/entity/user/auth/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.ecommerce.entity.user.auth;
+
+import com.ecommerce.entity.user.User;
+import com.ecommerce.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(
+                () -> new UsernameNotFoundException(email)
+        );
+        return new Principal(user);
+    }
+}
