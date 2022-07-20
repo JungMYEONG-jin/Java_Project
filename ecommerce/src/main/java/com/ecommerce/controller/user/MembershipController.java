8ae7e6550beda75285115f1c4e9e1b8fba2264b9
a60156077 <dwrhksfl77@naver.com>
2022-07-20 17:57:03 +0900

    ecommerce project
    - front 구현중

--- ecommerce/src/main/java/com/ecommerce/controller/user/MembershipController.java
+++ ecommerce/src/main/java/com/ecommerce/controller/user/MembershipController.java
@@ -1,4 +1,33 @@
 package com.ecommerce.controller.user;
 
+import com.ecommerce.entity.user.auth.Principal;
+import com.ecommerce.entity.user.dto.UserResDto;
+import com.ecommerce.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/membership")
+@RequiredArgsConstructor
 public class MembershipController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public String membershipForm(@AuthenticationPrincipal Principal principal, Model model){
+        model.addAttribute("user", UserResDto.of(principal.getUser()));
+        return "my/membership";
+    }
+
+    @PostMapping
+    public String membershipToggle(@AuthenticationPrincipal Principal principal, Model model){
+        UserResDto userResDto = userService.changeMembership(principal);
+        model.addAttribute("user", userResDto);
+        return "my/membership";
+    }
 }
