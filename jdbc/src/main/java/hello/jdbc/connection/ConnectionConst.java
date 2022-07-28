1a7d5d201274c70024c741aaa51af353feaec1cb
godcoder <dwrhksfl77@naver.com>
2022-07-28 12:50:09 +0900

    db 핵심 1편 chapter1

---
+++ jdbc/src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,8 @@
+package hello.jdbc.connection;
+
+// ABSTRACT 로 객체생성 막음
+public abstract class ConnectionConst {
+    public static final String URL = "jdbc:h2:tcp://localhost/~/testJdbc";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
+}
