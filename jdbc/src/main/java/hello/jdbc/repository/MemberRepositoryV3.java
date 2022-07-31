1599977f278a3fef1c4517051265b10aece13834
godcoder <dwrhksfl77@naver.com>
2022-07-31 10:32:37 +0900

    db 핵심 1편 chapter3 transaction template으로 교

---
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV3.java
@@ -0,0 +1,139 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.support.JdbcUtils;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.NoSuchElementException;
+
+/**
+ * Transaction Manager
+ * DataSourceUtils.getConnection
+ * DataSourceUtils.releaseConnection
+ */
+@Slf4j
+public class MemberRepositoryV3 {
+
+    private final DataSource dataSource; // use DI
+
+    public MemberRepositoryV3(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public Member save(Member member) throws SQLException{
+        String sql = "insert into member(member_id, money) values (?,?)";
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try{
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, member.getMemberId());
+            pstmt.setInt(2, member.getMoney());
+            pstmt.executeUpdate(); // run query
+
+            return member;
+        }catch (SQLException e){
+            log.error("db error", e);
+            throw e;
+        }finally {
+           close(con, pstmt, null);
+        }
+    }
+
+    public void update(String memberId, int money) throws SQLException {
+        String sql = "update member set money=? where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try{
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+            int cnt = pstmt.executeUpdate();// run query
+
+            log.info("cnt={}", cnt);
+
+        }catch (SQLException e){
+            log.error("db error", e);
+            throw e;
+        }finally {
+            close(con, pstmt, null);
+        }
+
+    }
+
+
+    public void delete(String memberId) throws SQLException {
+        String sql = "delete from member where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try{
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            pstmt.executeUpdate();
+        }catch (SQLException e){
+            log.error("db error", e);
+            throw e;
+        }finally {
+            close(con, pstmt, null);
+        }
+    }
+
+
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet res = null;
+
+        try{
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            res = pstmt.executeQuery();
+            if (res.next()){
+                Member member = new Member();
+                member.setMemberId(res.getString("member_id"));
+                member.setMoney(res.getInt("money"));
+                return member;
+            }else{
+                throw new NoSuchElementException("member not found memberId="+memberId);
+            }
+
+        }catch (SQLException e){
+            log.error("db error", e);
+            throw e;
+        }finally {
+            close(con, pstmt, res);
+        }
+    }
+
+    private Connection getConnection() throws SQLException {
+        // 트랜잭션 동기화 위해 DataSourceUtils use
+        Connection connection = DataSourceUtils.getConnection(dataSource);
+        log.info("get connection={}, class={}", connection, connection.getClass());
+        return connection;
+    }
+
+    // resource clear
+    private void close(Connection con, Statement stmt, ResultSet rs){
+        JdbcUtils.closeResultSet(rs);
+        JdbcUtils.closeStatement(stmt);
+        // 트랜잭션 동기화 사용을 위해
+        DataSourceUtils.releaseConnection(con, dataSource);
+//        JdbcUtils.closeConnection(con);
+    }
+
+
+}
