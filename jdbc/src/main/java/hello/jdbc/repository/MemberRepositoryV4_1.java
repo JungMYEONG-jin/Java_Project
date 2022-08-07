135d2c58f3dd2a2bf5f18bfb8914b9d2c5d0892c
godcoder <dwrhksfl77@naver.com>
2022-08-07 22:07:06 +0900

    interface와 runtime exception

---
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV4_1.java
@@ -0,0 +1,140 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.ex.MyDbException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.support.JdbcUtils;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.NoSuchElementException;
+
+/**
+ * 예외 누수 해결
+ * 체크 예외를 runtime으로 변경
+ * MemberRepository interface
+ */
+@Slf4j
+public class MemberRepositoryV4_1 implements MemberRepository{
+
+    private final DataSource dataSource; // use DI
+
+    public MemberRepositoryV4_1(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public Member save(Member member){
+        String sql = "insert into member(member_id, money) values (?,?)";
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try{
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, member.getMemberId());
+            pstmt.setInt(2, member.getMoney());
+            pstmt.executeUpdate(); // run query
+
+            return member;
+        }catch (SQLException e){
+            throw new MyDbException(e);
+        }finally {
+           close(con, pstmt, null);
+        }
+    }
+
+    @Override
+    public void update(String memberId, int money) {
+        String sql = "update member set money=? where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try{
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+            int cnt = pstmt.executeUpdate();// run query
+
+            log.info("cnt={}", cnt);
+
+        }catch (SQLException e){
+            throw new MyDbException(e);
+        }finally {
+            close(con, pstmt, null);
+        }
+
+    }
+
+
+    @Override
+    public void delete(String memberId)  {
+        String sql = "delete from member where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try{
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            pstmt.executeUpdate();
+        }catch (SQLException e){
+            throw new MyDbException(e);
+        }finally {
+            close(con, pstmt, null);
+        }
+    }
+
+
+
+    @Override
+    public Member findById(String memberId) {
+        String sql = "select * from member where member_id = ?";
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet res = null;
+
+        try{
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            res = pstmt.executeQuery();
+            if (res.next()){
+                Member member = new Member();
+                member.setMemberId(res.getString("member_id"));
+                member.setMoney(res.getInt("money"));
+                return member;
+            }else{
+                throw new NoSuchElementException("member not found memberId="+memberId);
+            }
+
+        }catch (SQLException e){
+            throw new MyDbException(e);
+        }finally {
+            close(con, pstmt, res);
+        }
+    }
+
+    private Connection getConnection() throws SQLException {
+        // 트랜잭션 동기화 위해 DataSourceUtils use
+        Connection connection = DataSourceUtils.getConnection(dataSource);
+        log.info("get connection={}, class={}", connection, connection.getClass());
+        return connection;
+    }
+
+    // resource clear
+    private void close(Connection con, Statement stmt, ResultSet rs){
+        JdbcUtils.closeResultSet(rs);
+        JdbcUtils.closeStatement(stmt);
+        // 트랜잭션 동기화 사용을 위해
+        DataSourceUtils.releaseConnection(con, dataSource);
+//        JdbcUtils.closeConnection(con);
+    }
+
+
+}
