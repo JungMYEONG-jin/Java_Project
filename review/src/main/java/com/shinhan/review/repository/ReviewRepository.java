a1fae38bae0d1c66f35ef1d85d48cda9d2d93488
a60156077 <dwrhksfl77@naver.com>
2022-09-19 10:39:38 +0900

    Add search function for app name..

--- review/src/main/java/com/shinhan/review/repository/ReviewRepository.java
+++ review/src/main/java/com/shinhan/review/repository/ReviewRepository.java
@@ -18,21 +18,49 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     // 전체 반환
     Page<Review> findAll(Pageable pageable);
-    // 시작 종료
-    @Query("select r from Review r where r.createdDate between :start and :end")
-    Page<Review> searchByDate(Pageable pageable, @Param("start") String start, @Param("end") String end); // 날짜 기반 검색
-    // 시작 종료 OS
-    @Query("select r from Review r where r.createdDate between :start and :end and r.osType = :type")
-    Page<Review> searchByDateAndOsType(Pageable pageable, @Param("start") String start, @Param("end") String end, @Param("type") String type); // 날짜 기반 검색
+
+    // 조건 1
     // OS로 검색
     Page<Review> findByOsType(Pageable pageable, String osType);
     // 시작일 검색
     Page<Review> findByCreatedDateAfter(Pageable pageable, String createdDate);
     // 종료일 검색
     Page<Review> findByCreatedDateBefore(Pageable pageable, String createdDate);
+    // app 이름
+    Page<Review> findByAppPkg(Pageable pageable, String appPkg);
+
+    // 조건 2
     // 시작 OS
     Page<Review> findByCreatedDateAfterAndOsType(Pageable pageable, String createdDate, String osType);
     // 종료 OS
     Page<Review> findByCreatedDateBeforeAndOsType(Pageable pageable, String createdDate, String osType);
+    // 시작 종료
+    @Query("select r from Review r where r.createdDate between :start and :end")
+    Page<Review> searchByDate(Pageable pageable, @Param("start") String start, @Param("end") String end); // 날짜 기반 검색
+    // 시작 app
+    Page<Review> findByCreatedDateAfterAndAppPkg(Pageable pageable, String createdDate, String appPkg);
+    // 종료 app
+    Page<Review> findByCreatedDateBeforeAndAppPkg(Pageable pageable, String createdDate, String appPkg);
+    // OS app
+    Page<Review> findByOsTypeAndAppPkg(Pageable pageable, String osType, String appPkg);
+
+    // 조건 3
+    // 시작 종료 OS
+    @Query("select r from Review r where r.createdDate between :start and :end and r.osType = :type")
+    Page<Review> searchByDateAndOsType(Pageable pageable, @Param("start") String start, @Param("end") String end, @Param("type") String type);
+    // 시작 종료 app
+    @Query("select r from Review r where r.createdDate between :start and :end and r.appPkg = :appPkg")
+    Page<Review> searchByDateAndAppPkg(Pageable pageable, @Param("start") String start, @Param("end") String end, @Param("appPkg") String appPkg);
+    // 시작 os app
+    Page<Review> findByCreatedDateAfterAndOsTypeAndAppPkg(Pageable pageable, String createdDate, String osType, String appPkg);
+    // 종료 os app
+    Page<Review> findByCreatedDateBeforeAndOsTypeAndAppPkg(Pageable pageable, String createdDate, String osType, String appPkg);
+
+    // 조건 4
+    // 시작 종료 os app
+    @Query("select r from Review r where r.createdDate between :start and :end and r.osType = :type and r.appPkg = :appPkg")
+    Page<Review> searchByDateAAndOsTypeAndAppPkg(Pageable pageable, @Param("start") String start, @Param("end") String end, @Param("type") String type, @Param("appPkg") String appPkg);
+
+
 }
 
