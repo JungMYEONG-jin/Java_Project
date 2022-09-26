695474b2a346142772aea9849a6598c06c32e42c
a60156077 <dwrhksfl77@naver.com>
2022-09-26 10:16:24 +0900

    h2 -> mysql

--- review/src/main/java/com/shinhan/review/ReviewApplication.java
+++ review/src/main/java/com/shinhan/review/ReviewApplication.java
@@ -2,14 +2,20 @@ package com.shinhan.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class ReviewApplication {
+public class ReviewApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReviewApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ReviewApplication.class);
+	}
 }
