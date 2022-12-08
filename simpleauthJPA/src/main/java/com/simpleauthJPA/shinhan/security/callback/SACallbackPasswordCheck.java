01f7c20770b6cb4fd7544e0c6bfa4944175c108e
MJ1996 <55305549+JungMYEONG-jin@users.noreply.github.com>
2022-12-08 22:51:56 +0900

    private

--- simpleauthJPA/src/main/java/com/simpleauthJPA/shinhan/security/callback/SACallbackPasswordCheck.java
+++
@@ -1,6 +0,0 @@
-package com.simpleauthJPA.shinhan.security.callback;
-
-public interface SACallbackPasswordCheck {
-
-    boolean CheckPasswordValidation(String password, String authtype);
-}
--- simpleauthJPA/src/main/java/com/simpleauthJPA/shinhan/security/callback/SACallbackSearch.java
+++
@@ -1,9 +0,0 @@
-package com.simpleauthJPA.shinhan.security.callback;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-
-public interface SACallbackSearch {
-    HashMap<String, String> cbSimpleAuthInfoSearch(HashMap<String, String> paramHashMap, HttpServletRequest paramHttpServletRequest, HttpServletResponse paramHttpServletResponse);
-}
