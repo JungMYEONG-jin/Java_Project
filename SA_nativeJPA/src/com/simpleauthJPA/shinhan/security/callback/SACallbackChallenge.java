fbfeb2b2c11e8a1dcd9161de63bd8769ec00351e
MJ1996 <55305549+JungMYEONG-jin@users.noreply.github.com>
2022-12-08 22:49:41 +0900

    비밀

--- SA_nativeJPA/src/com/simpleauthJPA/shinhan/security/callback/SACallbackChallenge.java
+++
@@ -1,5 +0,0 @@
-package com.simpleauthJPA.shinhan.security.callback;
-
-public interface SACallbackChallenge {
-    void cbSetChallengeInSession(String paramString);
-}
--- SA_nativeJPA/src/com/simpleauthJPA/shinhan/security/simpleauth/message/reg/SARegInitClientMessage.java
+++
@@ -1,9 +0,0 @@
-package com.simpleauthJPA.shinhan.security.simpleauth.message.reg;
-
-public class SARegInitClientMessage {
-    public String tag;
-
-    public String tmppub;
-
-    public String appid;
-}
